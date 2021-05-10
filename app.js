4391845ef1e9632d0c115a88164331269f1b3efc
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-09 22:44:15 -0500

    30. paquetes require

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const base = 5;
+
+const fs = require('fs');
+
+const base = 3;
 console.clear()
 console.log("Tabla del: "+base)
 
+let salida = '';
 
 for(i=1;i<=10;i++){
-    console.log(`${base} x ${i} = ${base*(i)}`)
+    salida+= `${base} x ${i} = ${base*(i)}\n`
 }
+
+fs.writeFile(`tabla-${base}.txt`,salida, (err)=>{
+    if(err) throw err;
+    console.log('archivo creado')
+})
+
+console.log(salida)
+
+
+
+
+
+
