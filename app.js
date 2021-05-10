0d7cfb30fc7d18959bf47424e5cec4841c2e460e
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-10 08:50:58 -0500

    32. recibir información desde linea de comando parte 1

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+/*   githut para promesas, decosntructores, callbacks y otros 
+https://github.com/Klerith/node-intro-javascript/blob/main/promesas.js
+*/
 
 const {crearArchivoPromise, crearArchivoAsyncAwait} = require('./helpers/multiplicar')
 
 const base = 5;
 console.clear()
 
-crearArchivoAsyncAwait(base)
+
+const [,,arg3='base=5'] = process.argv
+const [,baseArg=5] = arg3.split('=')
+console.log(baseArg)
+
+
+
+crearArchivoAsyncAwait(baseArg)
     .then(msg =>{
         console.log(msg+" creado")
     })
