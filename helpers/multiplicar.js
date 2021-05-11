074f05e356bd47c5ad899f701fa72e0569d9eb1e
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-10 20:10:26 -0500

    32. recibir información desde la linea de comandos parte 2

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,57 +1,56 @@
-
 const fs = require('fs');
 
-const crearArchivo = (base=5)=>{
+const crearArchivo = (base = 5) => {
 
-    console.log("Tabla del: ",base)
+    console.log("Tabla del: ", base)
 
-   
 
-    for(i=1;i<=10;i++){
-        salida+= `${base} x ${i} = ${base*(i)}\n`
+
+    for (i = 1; i <= 10; i++) {
+        salida += `${base} x ${i} = ${base*(i)}\n`
     }
 
-    fs.writeFileSync(`tabla-${base}.txt`,salida);
+    fs.writeFileSync(`tabla-${base}.txt`, salida);
     console.log('archivo creado');
     console.log(salida);
 }
 
 // con new promise
-const crearArchivoPromise = (base=5)=>{
-    
+const crearArchivoPromise = (base = 5) => {
 
-    return new Promise(( resolve, reject ) => {
 
-        console.log("Tabla del: ",base)
+    return new Promise((resolve, reject) => {
+
+        console.log("Tabla del: ", base)
         let salida = '';
 
-        
-        for(i=1;i<=10;i++){
-            salida+= `${base} x ${i} = ${base*(i)}\n`
+
+        for (i = 1; i <= 10; i++) {
+            salida += `${base} x ${i} = ${base*(i)}\n`
         }
-        fs.writeFileSync(`tabla-${base}.txt`,salida)
+        fs.writeFileSync(`tabla-${base}.txt`, salida)
 
         resolve(`tabla-${base}.txt`)
 
         console.log(salida);
 
-        
+
     });
 }
 
 
 // con async await
-const crearArchivoAsyncAwait = async(base=5)=>{
+const crearArchivoAsyncAwait = async(base = 5) => {
     try {
-        
-        console.log("Tabla del: ",base)
+
+        console.log("Tabla del: ", base)
         let salida = '';
 
-        
-        for(i=1;i<=10;i++){
-            salida+= `${base} x ${i} = ${base*(i)}\n`
+
+        for (i = 1; i <= 10; i++) {
+            salida += `${base} x ${i} = ${base*(i)}\n`
         }
-        fs.writeFileSync(`hola/ñ/tabla-${base}.txt`,salida)
+        fs.writeFileSync(`tabla-${base}.txt`, salida)
 
 
         console.log(salida);
@@ -60,12 +59,12 @@ const crearArchivoAsyncAwait = async(base=5)=>{
     } catch (error) {
         console.log(`Errormio ${error}`)
         throw error;
-        
-    }
-    
 
-        
-} 
+    }
+
+
+
+}
 
 module.exports = {
     crearArchivo,
