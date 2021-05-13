27b434927a40ce343b0becb41e7d111d8120df6b
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-12 21:11:20 -0500

    36. Yargs config independiente

--- app.js
+++ app.js
@@ -3,29 +3,11 @@ https://github.com/Klerith/node-intro-javascript/blob/main/promesas.js
 */
 
 const { crearArchivoPromise, crearArchivoAsyncAwait } = require('./helpers/multiplicar')
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        default: false
-    })
-    .check((argv, options) => {
-        if (isNaN(argv.b)) {
-            throw 'La base tiene que ser un número';
-        }
-        return true;
-    })
-    .argv;
+const argv = require('./config/yargs');
 
 // const base = 5;
 console.clear()
 
-console.log(argv)
 
 
 
